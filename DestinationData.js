0af86c5b1e4d9549af51d888402079df57f3ce88
B-Kavyasri <141469186+B-Kavyasri@users.noreply.github.com>
2023-09-26 15:06:54 +0530

    Add files via upload

---
+++ DestinationData.js
@@ -0,0 +1,23 @@
+import {Component} from "react";
+import "./DestinationStyles.css";
+import Destination from "./Destination";
+class DestinationData extends Component{ 
+    render(){
+       return(
+        <div className="first-des">
+            <div className="des-text">
+                <h2>{this.props.heading}</h2>
+                <p>
+                    {this.props.text}
+                </p>
+            </div>
+            <div className="image">
+               <img alt="img" src={this.props.img1}/>
+               <img alt="img" src={this.props.img2}/>  
+           </div>
+        </div>
+
+       ) ;
+    }
+}
+export default DestinationData;
